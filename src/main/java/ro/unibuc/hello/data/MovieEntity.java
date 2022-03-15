be4bbd00059c0c66a431ac12f52bb588ff5d0d27
FiroiAndrei <firoiandrei54@gmail.com>
2022-03-16 00:53:14 +0200

    Entities & Repos

---
+++ src/main/java/ro/unibuc/hello/data/MovieEntity.java
@@ -0,0 +1,53 @@
+package ro.unibuc.hello.data;
+
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Component
+public class MovieEntity {
+    @Id
+    public String id;
+    @Column(unique=true)
+    public String movieName;
+    public Date startTime;
+
+    public MovieEntity() {}
+
+    public MovieEntity(String id, String movieName, Date startTime) {
+        this.id = id;
+        this.movieName = movieName;
+        this.startTime = startTime;
+    }
+
+    public MovieEntity(String movieName, Date startTime) {
+        this.movieName = movieName;
+        this.startTime = startTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+}
